ba65c36b6759a6d9d044fec326b3cd5b973af5b0
Gui Melo <gui.smelo51@gmail.com>
2022-08-14 12:10:14 -0300

    feat(pecas): criacao de funçoes para cada peca

---
+++ xadrez/pecas.py
@@ -0,0 +1,20 @@
+import 
+class Pecas:
+    def __init__(self):
+    
+    def rainha(self,cor,local):
+        
+    def torre(self,cor,numero,local):
+
+    def bispo(self,cor,numero,local):
+
+    def cavalo(self,cor,numero,local):
+    
+    def rei(self,cor,local):
+    
+    def peao(self,cor,numero,local):
+    
+    def nula(self,local):
+    
+    
+
